945e7a2864c4f409c6956c95d46bb0bdc5ff1016
Elena Isaeva <isayeva.yelena.94@mail.ru>
2022-12-27 13:40:43 +0300

    added search tests

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -8,11 +8,11 @@ public class ProductManagerTest {
     public void testAdd() {
         ProductRepository repo = new ProductRepository();
         ProductManager manager = new ProductManager(repo);
-        Book book1 = new Book(1, "Война и мир", 1000 , "Лев Толстой");
-        Book book2 = new Book(2, "Мастер и Маргарита", 2000 , "Михаил Булгаков");
-        Book book3 = new Book(3, "Властелин колец", 3000 , "Джон Толкин");
+        Book book1 = new Book(1, "Война и мир", 1000, "Лев Толстой");
+        Book book2 = new Book(2, "Мастер и Маргарита", 2000, "Михаил Булгаков");
+        Book book3 = new Book(3, "Властелин колец", 3000, "Джон Толкин");
         Smartphone smartphone1 = new Smartphone(4, "Nokia", 5000, "Finland");
-        Smartphone smartphone2 = new Smartphone(5,"Samsung", 6000, "South Korea");
+        Smartphone smartphone2 = new Smartphone(5, "Samsung", 6000, "South Korea");
 
 
         manager.add(book1);
@@ -22,20 +22,20 @@ public class ProductManagerTest {
         manager.add(smartphone2);
 
         Product[] actual = repo.findAll();
-        Product[] expected = {book1, book2, book3,smartphone1,smartphone2};
+        Product[] expected = {book1, book2, book3, smartphone1, smartphone2};
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testSearchBy() {
+    public void testSearchByOne() {
         ProductRepository repo = new ProductRepository();
         ProductManager manager = new ProductManager(repo);
-        Book book1 = new Book(1, "Война и мир", 1000 , "Лев Толстой");
-        Book book2 = new Book(2, "Мастер и Маргарита", 2000 , "Михаил Булгаков");
-        Book book3 = new Book(3, "Властелин колец", 3000 , "Джон Толкин");
+        Book book1 = new Book(1, "Война и мир", 1000, "Лев Толстой");
+        Book book2 = new Book(2, "Мастер и Маргарита", 2000, "Михаил Булгаков");
+        Book book3 = new Book(3, "Властелин колец", 3000, "Джон Толкин");
         Smartphone smartphone1 = new Smartphone(4, "Nokia", 5000, "Finland");
-        Smartphone smartphone2 = new Smartphone(5,"Samsung", 6000, "South Korea");
+        Smartphone smartphone2 = new Smartphone(5, "Samsung", 6000, "South Korea");
 
         manager.add(book1);
         manager.add(book2);
@@ -50,5 +50,47 @@ public class ProductManagerTest {
     }
 
 
+    @Test
+    public void testSearchNotExistent() {
+        ProductRepository repo = new ProductRepository();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "Война и мир", 1000, "Лев Толстой");
+        Book book2 = new Book(2, "Мастер и Маргарита", 2000, "Михаил Булгаков");
+        Book book3 = new Book(3, "Властелин колец", 3000, "Джон Толкин");
+        Smartphone smartphone1 = new Smartphone(4, "Nokia", 5000, "Finland");
+        Smartphone smartphone2 = new Smartphone(5, "Samsung", 6000, "South Korea");
 
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
+
+        Product[] actual = manager.searchBy("Космос");
+        Product[] expected = {};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSearchBySome() {
+        ProductRepository repo = new ProductRepository();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "Война и мир", 1000, "Лев Толстой");
+        Book book2 = new Book(2, "Мастер и Маргарита", 2000, "Михаил Булгаков");
+        Book book3 = new Book(3, "Властелин колец", 3000, "Джон Толкин");
+        Smartphone smartphone1 = new Smartphone(4, "Nokia", 5000, "Finland");
+        Smartphone smartphone2 = new Smartphone(5, "Samsung", 6000, "South Korea");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
+
+        Product[] actual = manager.searchBy("и");
+        Product[] expected = {book1, book2, book3};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
